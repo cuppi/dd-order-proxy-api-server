5a88cc4238342ea862c3686ae337a56028781ac7
cuppi <cuppi413@163.com>
2019-05-22 14:29:29 +0800

    修改debug模式下compose配置

--- push_image.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker push swr.cn-east-2.myhuaweicloud.com/jinbaozheng/dd-order-proxy-api-server
---
+++ scripts/push_image.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker tag jinbaozheng/dd-order-proxy-api-server swr.cn-east-2.myhuaweicloud.com/jinbaozheng/dd-order-proxy-api-server
+docker push swr.cn-east-2.myhuaweicloud.com/jinbaozheng/dd-order-proxy-api-server
